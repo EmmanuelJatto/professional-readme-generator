cf8c063c539712c3ca26aa8bee5cc5dd97f7ada9
Emmanuel Jatto <emmanueljatto33@gmail.com>
2024-07-01 16:29:15 -0400

    completed challenge

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,143 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { title } = require('process');
+const { default: Choice } = require('inquirer/lib/objects/choice');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is the title of the project?', 'Enter description:' , 'Enter installation instructions:' , 'Enter usage information:' , 'Enter contribution guidelines:' , 'Enter test instructions:', 'Choose a license for the application:', 'What is your Github username?', 'What is your email address?'];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile('README.md', data, (fileName, err) => {
+        err ? console.log(err) : console.log(`${fileName} has been created!`)
+    })
+}
+
+const generateReadMe = (response) =>{
+    `#${response.title}
+
+    Table of Contents:
+        -(Description)[#Description]
+        -(Installation)[#Installation]
+        -(Usage)[#Usage]
+        -(Contributing)[#Contributing]
+        -(Tests)[#Tests]
+        -(License)[#License]
+        -(Questions)[#Questions]
+
+    #Description: 
+    ${response.description}
+
+    #Installation:
+    ${response.installationInstructions}
+
+    #Usage:
+    ${response.usageInformation}
+
+    #Contributing:
+    ${response.contribution}
+
+    #Tests:
+    ${response.testInstructions}
+
+    #License:
+    ${response.license}
+
+    #Questions:
+    Github Profile?
+    https://www.github.com/${response.username}
+    How can you reach me?
+    ${response.email}
+    `
+} 
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log('hello');
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: questions[0],
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: questions[1],
+        },
+        {
+            type: 'input',
+            name: 'installationInstructions',
+            message: questions[2],
+        },
+        {
+            type: 'input',
+            name: 'usageInformation',
+            message: questions[3],
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: questions[4],
+        },
+        {
+            type: 'input',
+            name: 'testInstructions',
+            message: questions[5],
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: questions[6],
+            choices: ['Public Domain License', 'LGPL', 'Permissive', 'Copyleft', 'Proprietary'],
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: questions[7],
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: questions[8],
+        },
+    ])
+    .then((response) => writeToFile('Generated ReadMe', `# ${response.title}
+
+    Table of Contents:
+        -(Description)[#Description]
+        -(Installation)[#Installation]
+        -(Usage)[#Usage]
+        -(Contributing)[#Contributing]
+        -(Tests)[#Tests]
+        -(License)[#License]
+        -(Questions)[#Questions]
+
+    #Description: 
+    ${response.description}
+
+    #Installation:
+    ${response.installationInstructions}
+
+    #Usage:
+    ${response.usageInformation}
+
+    #Contributing:
+    ${response.contribution}
+
+    #Tests:
+    ${response.testInstructions}
+
+    #License:
+    ${response.license}
+
+    #Questions:
+    Github Profile?
+    [Link to Github](https://www.github.com/${response.username})
+    How can you reach me?
+    ${response.email}
+    `))
+}
 
 // Function call to initialize app
 init();
